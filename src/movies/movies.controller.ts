192b5f03e03c481c877be784a82de9fabf6fc965
soo-sin <pcs7993@gmail.com>
2021-01-05 07:56:53 +0900

    학습중

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+
+@Controller('movies')
+export class MoviesController {
+  //nestJS에서는 모델을 가져올 때 이렇게 가져옴
+  constructor(private readonly moviesService: MoviesService) {}
+
+  @Get()
+  getAll(): Movie[] {
+    return this.moviesService.getAll();
+  }
+
+  @Get('search') //search가 id보다 밑에 있으면 search를 id로 판단할 수 있으니 주의
+  search(@Query('year') searchingYear: string) {
+    return `we are searching for a movie title : ${searchingYear}`;
+  }
+
+  @Get('/:id')
+  getOne(@Param('id') movieId: number): Movie {
+    console.log(typeof movieId);
+    return this.moviesService.getOne(movieId);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.create(movieData);
+  }
+
+  @Delete('/:id')
+  remove(@Param('id') movidId: number) {
+    return this.moviesService.deleteOne(movidId);
+  }
+
+  @Patch('/:id')
+  path(@Param('id') movieId: number, @Body() updateData) {
+    return this.moviesService.update(movieId, updateData);
+  }
+}
---
+++ src/movies/movies.service.ts
@@ -0,0 +1,37 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Movie } from './entities/movie.entity';
+import { CreateMovieDto } from './dto/create-movie.dto';
+
+@Injectable()
+export class MoviesService {
+  private movies: Movie[] = [];
+
+  getAll(): Movie[] {
+    return this.movies; //실제로는 query
+  }
+
+  getOne(id: number): Movie {
+    const movie = this.movies.find((movie) => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`movie with ID : ${id} not found`); //NESTJS에서 제하는 예외처리
+    }
+    return movie;
+  }
+
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== id);
+  }
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
+  }
+
+  update(id: number, updateData) {
+    const movie = this.getOne(id); //다른 함수에서써서 예외처리를 쉽게.
+    this.deleteOne(id);
+    this.movies.push({ ...movie, ...updateData });
+  }
+}
